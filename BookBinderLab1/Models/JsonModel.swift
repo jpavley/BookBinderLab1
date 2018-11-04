9f3fd60f55bc69ddeec194ff5298b99c5497d90c
John Pavley <jpavley@gmail.com>
2018-11-04 10:11:58 -0500

    documented JSON model

--- BookBinderLab1/Models/JsonModel.swift
+++ BookBinderLab1/Models/JsonModel.swift
@@ -9,39 +9,66 @@
 import Foundation
 
 /// Models a BookBinder Series with Works and Variants in JSON.
-/// Sample JOSN code:
+/// - `Publisher` has a name and contains a list of series.
+/// - `Series` has a title, a list of characters, and a list of volumes.
+/// - `Volume` has a kind (publishing frequency), era (year), number, set of properties used to
+///   calculate uncollected works (firstWorkNumber, currentWorkNumber, list of skippedWorkNumbers),
+///   and a list of works.
+/// - `Work` has a number and a list of variants.
+/// - `Variant` has a date published, kind (publishing media), printing, letter, coverID,
+///   dateCollected, and dateConsumed (consumption properties used to determin ownership and
+///   tracking.
+/// - *Note: each work has a URI, which are used as keys to identify works in a dictionary but
+///   URIs are calucated at runtime so not serialized in JSON except for the selectedURI which
+///   lets an app know what the user was looking at the time of serialization.*
 /// ```
 /// {
-///      "selectedURI": "",
-///      "series":
-///      [
-///          {
-///              "seriesPublisher": "",
-///              "seriesTitle": "",
-///              "seriesEra": 0,
-///              "seriesVolume": 1,
-///              "seriesFirstWork": 0,
-///              "seriesCurrentWork": 0,
-///              "seriesSkippedWorks": [
-///                 0
-///             ],
-///              "works":
-///              [
-///                  {
-///                      "workNumber": 0,
-///                      "variants":
-///                      [
-///                          {
-///                              "printing": 0,
-///                              "letter": "",
-///                              "coverImageID": "",
-///                              "isOwned": true
-///                          }
-///                      ]
-///                  }
-///              ]
-///          }
-///      ]
+///     "selectedURI": "1/publisher-name/series-title/volume era/work-number/variant-letter",
+///     "publisher": [
+///         {
+///             "name": "",
+///             "series": [
+///                 {
+///                     "title": "",
+///                     "characters": [
+///                         {
+///                             "name": ""
+///                         }
+///                     ],
+///                     "volumes": [
+///                         {
+///                             "kind": "ongoing, miniseries, one-shot, annual",
+///                             "era": 0,
+///                             "number": 0,
+///                             "firstWorkNumber": 0,
+///                             "currentWorkNumber": 0,
+///                             "skippedWorkNumbers": [
+///                                 {
+///                                     "number": 0
+///                                 }
+///                             ],
+///                             "works": [
+///                                 {
+///                                     "number": 0,
+///                                     "variants": [
+///                                         {
+///                                             "datePublished": "",
+///                                             "kind": "magazine, trade, digital",
+///                                             "printing": 0,
+///                                             "letter": "",
+///                                             "coverID": "",
+///                                             "dateColleted": "",
+///                                             "dateConsumed": ""
+///                                         }
+///                                     ]
+///                                 }
+///                             ]
+///                         }
+///                     ]
+///                 }
+///             ]
+///         }
+///     ]
 /// }
 /// ```
 struct JsonModel: Codable {
